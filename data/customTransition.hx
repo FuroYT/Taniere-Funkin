8ea7cec1757930b7b056dae4a2fe01f79971abcb
JustAnto <AchrafAfid+1@gmail.com>
2025-10-26 19:04:24 +0100

    Update customTransition.hx

--- data/customTransition.hx
+++ data/customTransition.hx
@@ -1,23 +1,3 @@
-function gravityEasing(t) {
-    // The second comment is going to translate for those who don't speak french
-
-    // J'aime bien expoIn donc je le prends comme base d'acceleration
-    // I like expoIn so I'm going to use it as an acceleration base
-    var expoInValue = FlxEase.expoIn(t);
-    
-    // Après la on va faire un easing qui ressemble à la gravité de la terre pour que ca rende assez naturel
-    // After that we're going to make an easing that looks like earth's gravity so it looks natural
-    var gravityValue = t * t * (3 - 2 * t);  // Tema l'easing de fou // That easing is crazy
-    
-    // Ease de transition entre expoIn et la gravité
-    // Transition easing between expoIn and gravity
-    var smoothTransition = t * t * (3 - 2 * t);  
-
-    //On applique toute la merde ensemble
-    //We apply all the shit together
-    return expoInValue * (1 - smoothTransition) + gravityValue * smoothTransition;
-}
-
 var spr;
 var spikes;
 function create(event)
@@ -33,13 +13,7 @@ function create(event)
     {
         case true:
             spr.y = -(spr.height + spikes.height);
-            FlxTween.tween(spr, {y: - (FlxG.height * FlxG.random.float(0.4, 0.6))}, FlxG.random.float(0.5, 0.9), {ease: FlxEase.circOut});
-            new FlxTimer().start(0.9, (t) -> {
-                FlxTween.tween(spr, {y: 0}, 0.4, {ease: gravityEasing});
-                new FlxTimer().start(0.6, (t) -> {
-                    finish();
-                });
-            });
+            FlxTween.tween(spr, {y: 0}, 0.5, {ease: FlxEase.quartOut, onComplete: finish});
         case false:
             spikes.flipY = true;
             FlxTween.tween(spr, {y: FlxG.height + spikes.height}, FlxG.random.float(1, 1.4), {ease: FlxEase.expoOut});
