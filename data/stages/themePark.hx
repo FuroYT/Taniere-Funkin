ebb1fa67425ddf28b8dd9224ca5544289d979b04
LéaNimatics <ptibourguiz@gmail.com>
2025-10-26 20:25:28 +0100

    Update themePark.hx

--- data/stages/themePark.hx
+++ data/stages/themePark.hx
@@ -6,11 +6,15 @@ function create() {
     insert(members.indexOf(gf)+1,bg);
     bg.antialiasing = true;
     bg.velocity.x = 200;
+    trace(dad.y+20);
 }
 
 function beatHit(curBeat:Int) {
-    if(tweenBf != null) tweenBf.cancel();
-    if(tweenDad != null) tweenDad.cancel();
-    tweenBf = FlxTween.tween(bf, {y:bf.y+20}, 1, {type:FlxTween.BACKWARD, ease:FlxEase.expoOut});
-    tweenDad = FlxTween.tween(dad, {y:dad.y+20}, 1, {type:FlxTween.BACKWARD, ease:FlxEase.expoOut});
+    bf.y = 100; 
+    dad.y = 100; 
+}
+
+function update(elapsed:Float) {
+        bf.y = FlxMath.lerp(bf.y, 0, elapsed/2.5); 
+        dad.y = FlxMath.lerp(dad.y, 0, elapsed/2.5); 
 }
